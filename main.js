379d91548ac82532b21849350a8365967442412d
Andreili1000 <60061677+Andreili1000@users.noreply.github.com>
2020-07-08 22:38:39 +0200

    added command interpreter

--- main.js
+++ main.js
@@ -75,7 +75,8 @@ class Hydrawise extends utils.Adapter {
         Here a simple template for a boolean variable named "testVariable"
         Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
         */
-        await this.setObjectNotExistsAsync('testVariable', {
+
+        /* await this.setObjectNotExistsAsync('testVariable', {
             type: 'state',
             common: {
                 name: 'testVariable',
@@ -86,34 +87,39 @@ class Hydrawise extends utils.Adapter {
             },
             native: {},
         });
+        */
 
         // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
+        // this.subscribeStates('testVariable');
         // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
         // this.subscribeStates('lights.*');
         // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
         // this.subscribeStates('*');
+
+        this.subscribeStates('command');
 
         /*
             setState examples
             you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
         */
         // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
+
+        // -- await this.setStateAsync('testVariable', true);
 
         // same thing, but the value is flagged "ack"
         // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
+
+        // -- await this.setStateAsync('testVariable', { val: true, ack: true });
 
         // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+
+        // -- await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
 
         // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        // let result = await this.checkPasswordAsync('admin', 'iobroker');
+        // this.log.info('check user admin pw iobroker: ' + result);
+        // result = await this.checkGroupAsync('admin', 'admin');
+        // this.log.info('check group user admin group admin: ' + result);
     }
 
     /**
@@ -157,9 +163,35 @@ class Hydrawise extends utils.Adapter {
      * @param {ioBroker.State | null | undefined} state
      */
     onStateChange(id, state) {
+
         if (state) {
             // The state was changed
             this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+
+            switch (id){
+              //
+              // execute hydrawise command
+              //
+              case adapter.namespace + '.command':
+
+                // lese custom_run, custom_suspend, zone ein
+                var custom_run = getState(adapter.namespace + '.custom_run').val;
+                var custom_suspend = getState(adapter.namespace + '.custom_suspend').val;
+                var custom_zone = getState(adapter.namespace + '.custom_zone').val;
+                this.log.info(`custom_run = ${custom_run}, custom_suspend = ${custom_suspend}, zone = ${zone}`);
+
+                // evaluate command
+                switch (state.val){
+                  case "run": this.log.info("run");break;
+                  case "stop": this.log.info("stop");break;
+                  case "suspend": this.log.info("suspend");break;
+                  case "runall": this.log.info("runall");break;
+                  case "stopall": this.log.info("stopall");break;
+                  case "suspendall": this.log.info("suspendall");break;
+                }
+                break;
+            }
+
         } else {
             // The state was deleted
             this.log.info(`state ${id} deleted`);
